caa311cad45e6c930649f2189d9d1273facf6bec
Ayush_Raj <140777065+AyushRajgit@users.noreply.github.com>
2025-08-05 22:51:41 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -85,15 +85,6 @@ const Home = () => {
             </div>
           </div>
         </div>
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className={`w-6 h-10 border-2 rounded-full flex justify-center ${
-            isDarkMode ? 'border-gray-400' : 'border-gray-600'
-          }`}>
-            <div className="w-1 h-3 bg-blue-500 rounded-full mt-2 animate-pulse"></div>
-          </div>
-        </div>
       </section>
 
       {/* Features Section */}
